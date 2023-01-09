adeea8b13fbb2405b4bef10f8cd035466303f1b9
Paul Miller <paul@paulmillr.com>
2023-01-09 19:08:39 +0000

    Lint

--- index.ts
+++ index.ts
@@ -241,7 +241,7 @@ export class HDKey {
         const added = secp.Point.fromHex(this.pubKey).add(secp.Point.fromPrivateKey(childTweak));
         // Cryptographically impossible: hmac-sha512 preimage would need to be found
         if (added.equals(secp.Point.ZERO)) {
-          throw new Error('The tweak was equal to negative P, which made the result key invalid')
+          throw new Error('The tweak was equal to negative P, which made the result key invalid');
         }
         opt.publicKey = added.toRawBytes(true);
       }
