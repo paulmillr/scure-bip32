f51f0edf335f9e2241ab9e533bf9fdc75557bf21
quapka <>
2025-04-05 16:05:00 +0200

    Unwanted whitespace clean up

--- test/hdkey.test.js
+++ test/hdkey.test.js
@@ -128,7 +128,6 @@ describe('hdkey', () => {
       // but deriving one level deeper fails.
       throws(() => hdkey.derive('m' + '/0'.repeat(256)));
     });
-
     it('Should derive private key correctly', () => {
         const seed = 'fffcf9f6f3f0edeae7e4e1dedbd8d5d2cfccc9c6c3c0bdbab7b4b1aeaba8a5a29f9c999693908d8a8784817e7b7875726f6c696663605d5a5754514e4b484542';
         const hdkey = HDKey.fromMasterSeed(hexToBytes(seed));
